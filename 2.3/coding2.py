377c66ebd6269f07c8459e1ceeaac830df462646
Mr. Chin <jchin@vsb.bc.ca>
2025-09-22 16:02:43 -0700

    Added 2.3 questions
    * 2.3 coding self-test questions

---
+++ 2.3/coding2.py
@@ -0,0 +1,16 @@
+"""
+Write an Olympic Judging program that outputs the average scores from 5 different judges. 
+
+Each score is out of 10 points maximum. Half points are allowed (e.g. 7.5)
+
+The program should take 5 inputs and output the final average score.
+
+Example:
+
+Judge 1: 5.5
+Judge 2: 10
+Judge 3: 7
+Judge 4: 8.5
+Judge 5: 9
+Your Olympic score is 8.0
+"""
