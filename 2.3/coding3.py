9d36b9652a593dd216101e438ddcf976319ae774
Mr. Chin <jchin@vsb.bc.ca>
2025-09-22 03:03:57 +0000

    2.3 coding self-test questions

---
+++ 2.3/coding3.py
@@ -0,0 +1,27 @@
+"""
+Write a McDoland's program that takes your order and outputs the total cost.
+
+It first asks if you want a burger for $5. It then asks if you want fries for $3. It outputs the total with 14% tax.
+
+The program should accept Yes/No or yes/no.
+
+Example:
+
+Would you like a burger for $5? (Yes/No)
+> yes
+Would you like fries for $3? (Yes/No)
+> yes
+Your total is $9.12
+
+Would you like a burger for $5? (Yes/No)
+> yes
+Would you like fries for $3? (Yes/No)
+> no
+Your total is $5.699999999999999
+
+Would you like a burger for $5? (Yes/No)
+> no
+Would you like fries for $3? (Yes/No)
+> yes
+Your total is $3.42
+"""
