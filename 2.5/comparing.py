aff6617de2e23fff800d045d9192fb099e806ab3
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-09-29 22:04:25 +0000

    hehe

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,17 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+
+
+#we will be making a data analysis page
+#it will use the data that we submitted trhough the survey
+#the user input will ask who has which topic in common, have the same answers as whoever
+
+#Data Analysis
+#Ysabelle Guzman
+#Sep 29 2025
+
+#FILE OPENER
+file = open("responses.csv")
+file.readline()
+
