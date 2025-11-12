2e60baaea1310fe5f549298f6e5072957b103812
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-11-12 21:34:51 +0000

    help

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,21 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+
+def colours(r, g, b):
+    if 0 >= r <= 255 and 0 >= g <= 255 and 0 >= b <= 255:
+        return True
+    else:
+        return False
+
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
+
+print(image_green[0,0])
+
+pixel = image_green[0,0]
+r = pixel[0]
+g = image_green[0,0][1]
+b = image_green[0,0][2]
+
+print(colours(r, g, b))
+
