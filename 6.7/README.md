109ff00bd6a6eae0a8ee85d29a532b519f599f7c
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-12-01 21:28:01 +0000

    2ndcommit, need to make function for yes.

--- 6.7/README.md
+++ 6.7/README.md
@@ -3,5 +3,7 @@
 ***MY PROGRAM***
 In my project, there will be 10 images that the program that
 will detect if either it is night time (dark colours like dark blue or black)
-or day time (bright colours like light blue or white).
+or day time (bright colours like light blue or white). The program should say that theyre is 5 of each time that represents
+its requirements.
+
 
--- 6.7/main.py
+++ 6.7/main.py
@@ -8,3 +8,44 @@
 
 import time
 
+from PIL import Image
+
+t0 = time.time()
+
+def colour(r, g, b):
+    # r <= 235 g <= 48 b <= 89
+    if r <= 235 and g <= 48 and b <= 89:
+        return "day"
+    # r <= 230 g <= 50 b <= 10
+    elif r <= 230 and g <= 50 and b <= 10:
+        return "night"
+    
+file = Image.open("6.7/day1.jpg")
+t1 = time.time()
+
+file = Image.open("6.7/day2.jpg")
+
+file = Image.open("6.7/day3.jpg")
+
+file = Image.open("6.7/day4.jpg")
+
+
+width = file.width
+height = file.height
+
+
+for x in range(width):
+    for y in range(height):
+        pixel_r = jb_image[x,y][0]
+        pixel_g = jb_image[x,y][1]
+        pixel_b = jb_image[x,y][2]
+
+        if colour(pixel_r, pixel_g, pixel_b) == "day":
+            yellow_pixels.append(jb_image[x,y])
+            file.putpixel((x, y), (255, 255, 255))
+        if colour(pixel_r, pixel_g, pixel_b) == "night":
+            red_pixels.append(jb_image[x,y])
+           orange_pixels.append(jb_image[x,y])
+
+#images will loop into the function
+    file = file.open("6.7/")
