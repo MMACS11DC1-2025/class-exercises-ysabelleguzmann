085d9215cd3b3fabb46806ae30f1141b2643a82f
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-12-02 21:11:03 +0000

    3rd commit

--- 6.7/main.py
+++ 6.7/main.py
@@ -24,28 +24,51 @@ file = Image.open("6.7/day1.jpg")
 t1 = time.time()
 
 file = Image.open("6.7/day2.jpg")
+t2 = time.time()
 
 file = Image.open("6.7/day3.jpg")
 
 file = Image.open("6.7/day4.jpg")
 
+file = Image.open("6.7/day5.jpg")
+
+file = Image.open("6.7/night1.jpg")
+
+file = Image.open("6.7/night2.jpg")
+
+file = Image.open("6.7/night3.jpg")
+
+file = Image.open("6.7/night4.jpg")
+
+file = Image.open("6.7/night5.jpg")
+
+
+dn_image = file.load()
 
 width = file.width
 height = file.height
 
+day_pixels = []
+night_pixels = []
 
 for x in range(width):
     for y in range(height):
-        pixel_r = jb_image[x,y][0]
-        pixel_g = jb_image[x,y][1]
-        pixel_b = jb_image[x,y][2]
+        pixel_r = dn_image[x,y][0]
+        pixel_g = dn_image[x,y][1]
+        pixel_b = dn_image[x,y][2]
 
         if colour(pixel_r, pixel_g, pixel_b) == "day":
-            yellow_pixels.append(jb_image[x,y])
-            file.putpixel((x, y), (255, 255, 255))
+            day_pixels.append(dn_image[x,y])
         if colour(pixel_r, pixel_g, pixel_b) == "night":
-            red_pixels.append(jb_image[x,y])
-           orange_pixels.append(jb_image[x,y])
+            night_pixels.append(dn_image[x,y])
 
-#images will loop into the function
-    file = file.open("6.7/")
+num_day = len(day_pixels)
+num_night = len(night_pixels)
+
+total_pixels = width*height
+
+day_ratio = total_pixels / num_day
+night_ratio = total_pixels / num_night
+
+day_percent = day_ratio * 100
+night_percent = night_ratio * 100
