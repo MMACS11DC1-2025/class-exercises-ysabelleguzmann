a40cc350cf00ec16515947f17570f9f94ddfca26
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-12-03 21:33:48 +0000

    donezos

--- 6.2/search.py
+++ 6.2/search.py
@@ -15,3 +15,19 @@ for line in file:
 print("Dance score \tSong")
 for item in drake_data:
 	print(str(item[0]) + "\t\t" + item[1] + " by " + item[2])
+
+#lowest to highest
+
+for i in range(len(drake_data)):
+	lowest = drake_data[i]
+	lowest_index = i
+
+	for j in range(i + 1, len(drake_data)):
+		if drake_data[j] > lowest:
+			lowest = drake_data[j]
+			lowest_index = j
+
+	drake_data[lowest_index], drake_data[i] = drake_data[i], drake_data[lowest_index]
+
+
+print(drake_data)
