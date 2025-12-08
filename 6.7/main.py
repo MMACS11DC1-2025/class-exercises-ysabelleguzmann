a91f3238d600ee5d1f737c8bd6b17badefdaa545
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-12-08 21:34:33 +0000

    comimit

--- 6.7/main.py
+++ 6.7/main.py
@@ -20,55 +20,31 @@ def colour(r, g, b):
     elif r <= 230 and g <= 50 and b <= 10:
         return "night"
     
-file = Image.open("6.7/day1.jpg")
-t1 = time.time()
+world = ["6.7/day1.jpg", "6.7/day2.jpg", "6.7/day3.jpg", "6.7/day4.jpg", "6.7/day5.jpg", "6.7/night1.jpg", "6.7/night2.jpg", "6.7/night3.jpg", "6.7/night4.jpg", "6.7/night5.jpg"]
 
-file = Image.open("6.7/day2.jpg")
-t2 = time.time()
+for image in world:
+    file = Image.open(world)
+    dn_image = file.load()
 
-file = Image.open("6.7/day3.jpg")
+for i in range(len(world)):
+    width = file.width
+    height = file.height
 
-file = Image.open("6.7/day4.jpg")
+    day_pixels = []
+    night_pixels = []
 
-file = Image.open("6.7/day5.jpg")
+    for x in range(width):
+        for y in range(height):
+            pixel_r = dn_image[x,y][0]
+            pixel_g = dn_image[x,y][1]
+            pixel_b = dn_image[x,y][2]
 
-file = Image.open("6.7/night1.jpg")
-
-file = Image.open("6.7/night2.jpg")
-
-file = Image.open("6.7/night3.jpg")
-
-file = Image.open("6.7/night4.jpg")
-
-file = Image.open("6.7/night5.jpg")
-
-
-dn_image = file.load()
-
-width = file.width
-height = file.height
-
-day_pixels = []
-night_pixels = []
-
-for x in range(width):
-    for y in range(height):
-        pixel_r = dn_image[x,y][0]
-        pixel_g = dn_image[x,y][1]
-        pixel_b = dn_image[x,y][2]
-
-        if colour(pixel_r, pixel_g, pixel_b) == "day":
-            day_pixels.append(dn_image[x,y])
-        if colour(pixel_r, pixel_g, pixel_b) == "night":
-            night_pixels.append(dn_image[x,y])
+            if colour(pixel_r, pixel_g, pixel_b) == "day":
+                day_pixels.append(dn_image[x,y])
+            if colour(pixel_r, pixel_g, pixel_b) == "night":
+                night_pixels.append(dn_image[x,y])
 
 num_day = len(day_pixels)
 num_night = len(night_pixels)
 
-total_pixels = width*height
-
-day_ratio = total_pixels / num_day
-night_ratio = total_pixels / num_night
-
-day_percent = day_ratio * 100
-night_percent = night_ratio * 100
+total_day = num_day * 100
