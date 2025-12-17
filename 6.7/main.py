6737b91aadb9e9a15401c89de8cfc2256b1500d4
ysabelleguzmann <1535043@learn.vsb.bc.ca>
2025-12-17 06:07:06 +0000

    hooray

--- 6.7/main.py
+++ 6.7/main.py
@@ -2,14 +2,13 @@
 #ysabelle guzman
 #nov 26 25
 
-
-#START OFF WITH A FUNCTION THAT GOES THROUGH EACH PIXEL PER PICTURE AND DEFINING ITS COLOUR
-#TRUE IF FOLLOWS ALONG WITH THE REQUIREMENTS,
-#ELSE = FALSE
-
 import time
 
 from PIL import Image
+
+#list of day/night images 
+world = ["6.7/day1.jpg", "6.7/day2.jpg", "6.7/day3.jpg", "6.7/day4.jpg", "6.7/day5.jpg", "6.7/night1.jpg", "6.7/night2.jpg", "6.7/night3.jpg", "6.7/night4.jpg", "6.7/night5.jpg"]
+percentage = []
 
 #defines the colour that represents day, and that represents night
 def colour(r, g, b):
@@ -24,112 +23,103 @@ def colour(r, g, b):
         return "night"
     else:
         return None
-    
-#list of day/night images 
-world = ["6.7/day1.jpg", "6.7/day2.jpg", "6.7/day3.jpg", "6.7/day4.jpg", "6.7/day5.jpg", "6.7/night1.jpg", "6.7/night2.jpg", "6.7/night3.jpg", "6.7/night4.jpg", "6.7/night5.jpg"]
-percentage = []
 
-#totals for number of images
-day_images = 0
-night_images = 0
-
-t0 = time.time()
 
 #loops through each image
-for i in range(len(world)):
-    pictures = world[i]
-    file = Image.open(pictures)
-    dn_image = file.load()
+for img in world:
+    t0 = time.time()
+
+    file = Image.open(img)
+    file = file.resize((200, 200)) #Defaults each image into 200x200
+    pixels = file.load()
 
     width = file.width
     height = file.height
 
+    total_pixels = width*height
+
     # pixel counter
-    db_pixels = []
-    b_pixels = []
-    night_pixels = []
+    day_pixels = 0
+    night_pixels = 0
 
 #checks if pixel matches day or night
     for x in range(width):
         for y in range(height):
-            pixel_r = dn_image[x,y][0]
-            pixel_g = dn_image[x,y][1]
-            pixel_b = dn_image[x,y][2]
+            r, g, b = pixels[x, y][:3]
+            result = colour(r, g, b)
 
-            if colour(pixel_r, pixel_g, pixel_b) == "dark blue":
-                db_pixels.append(dn_image[x,y])
-                file.putpixel((x, y), (255, 255, 255))
-            if colour(pixel_r, pixel_g, pixel_b) == "blue":
-                b_pixels.append(dn_image[x,y])
-            if colour(pixel_r, pixel_g, pixel_b) == "night":
-                night_pixels.append(dn_image[x,y])
+            if result == "blue" or result == "dark blue":
+                day_pixels += 1
+            elif result == "night":
+                night_pixels += 1
 
-t1 = time.time()
+    day_percent = (day_pixels / total_pixels) * 100
+    t1 = time.time()
+    total_time = t1 - t0
 
-num_DB = len(db_pixels)
-num_b = len(b_pixels)
-num_night = len(night_pixels)
+    percentage.append([day_percent, img, total_time])
 
-total_pixels = width * height
+    print(f"{img}")
+    print(f"    DAY: {day_percent:.2f}%")
+    print(f"    TIME: {total_time:.2f} seconds\n")
 
-day_ratio = num_DB + num_b / total_pixels
-night_ratio = num_night / total_pixels
-
-
-total_time = t1 - t0
-
-timing = "it took {:.2f}s to run this".format(total_time)
-print(timing)
-
-#total_day = n
-#total_night = num_night
-
-if total_day > 0:
-    day_percentage = total_night / * 100
-
-output = "Images {}, it is {:.2f}% day.".format(world[i], day_percentage)
-print(output)
 
 # UNIT 6: SORTING SECTION
 # in this section, it will sort from the brightest to darkest image.
 
-for i in range(i + 1, len(percentage)):
+#SELECTION SORT
+for i in range(len(percentage)):
     largest_index = i
-
-    for j in range(i + j, len(percentage)):
+    for j in range(i + 1, len(percentage)):
         if percentage[j][0] > percentage[largest_index][0]:
             largest_index = j
 
     percentage[i], percentage[largest_index] = percentage[largest_index], percentage[i]
 
-    print("\n Top 5 most burnt breads are: ")
-    for i in percentage[:5]:
-        print(str(i[1]) + " with " + f"{i[0]:.2f}% " + " burn. ")
+# print sorted results
+print("Images sorted from most day (brightest) to most night (darkest):\n")
+for item in percentage:
+    print(f"{item[1]} has {item[0]:.2f}% day")
 
-def search(list_of_lists, query):
-    querymin = query - 5
-    search_start_index = 0
-    search_end_index = len(list_of_lists) - 1
+#BINARY SEARCH
+def binary_search_day(images, target):
 
-    while search_start_index <= search_end_index:
-        midpoint = int((search_start_index + search_end_index) / 2)
+    low = 0
+    high = len(images) - 1
 
-        if list_of_lists[midpoint][0] <= query and list_of_lists[midpoint][0] >= querymin:
-            return list_of_lists[midpoint][1]
+    lower = target - 1
+    higher = target + 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        mid_value = images[mid][0]
         
-        elif list_of_lists[midpoint][0] > query and list_of_lists[midpoint][0] >= querymin:
-            search_start_index = midpoint + 1
+        if lower <= mid_value <= higher:
+            return images[mid]
+        
+        elif mid_value < lower:
+            high = mid - 1
 
         else:
-            searchend_index = midpoint - 1
+            low = mid + 1
 
-search_target = 100.00
-found_target = search(percentage, search_target)
+    return None
+   
 
-if found_target:
-    print("\n: {}".format(search_target, found_target))
+search_target = 33.0
+
+result = binary_search_day(percentage, search_target)
+
+
+if result:
+    print("\nBinary Search Results:")
+    print(f"IMAGE: {result[1]}")
+    print(f"DAY: {result[0]:.2f}% ")
+    print(f"TIME: {result[2]:.2f}s")
+
+
 else:
-    print("\n {:.2f}% ".format(search_target))
+    print("\nNo image found near {search_target:.2f}% day")
 
 
     
